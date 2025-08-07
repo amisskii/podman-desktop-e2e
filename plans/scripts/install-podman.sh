79876276f0293d5b9a8abf76ed0f508102e17e78
Anton Misskii <124462506+amisskii@users.noreply.github.com>
2025-08-07 15:50:28 +0200

    Update install-podman.sh

--- plans/scripts/install-podman.sh
+++ plans/scripts/install-podman.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -eu
-
+sudo dnf repolist
 COMPOSE_VERSION="fc$(echo "$COMPOSE" | cut -d '-' -f 2)"
 CUSTOM_PODMAN_URL="https://kojipkgs.fedoraproject.org//packages/podman/${PODMAN_VERSION}/1.${COMPOSE_VERSION}/${ARCH}/podman-${PODMAN_VERSION}-1.${COMPOSE_VERSION}.${ARCH}.rpm"
 
 if [[ $PODMAN_VERSION == "latest" ]]; then
     sudo dnf install -y podman
+    echo "??????????"
+    podman --version
 else
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     echo $CUSTOM_PODMAN_URL
