79503700eda15452d5e0d8e77e0484a3f6f6560c
Anton Misskii <124462506+amisskii@users.noreply.github.com>
2025-08-07 16:01:57 +0200

    Update install-podman.sh

--- plans/scripts/install-podman.sh
+++ plans/scripts/install-podman.sh
@@ -5,6 +5,8 @@ COMPOSE_VERSION="fc$(echo "$COMPOSE" | cut -d '-' -f 2)"
 CUSTOM_PODMAN_URL="https://kojipkgs.fedoraproject.org//packages/podman/${PODMAN_VERSION}/1.${COMPOSE_VERSION}/${ARCH}/podman-${PODMAN_VERSION}-1.${COMPOSE_VERSION}.${ARCH}.rpm"
 
 if [[ $PODMAN_VERSION == "latest" ]]; then
+    sudo dnf remove podman
+    sudo dnf install podman --disablerepo=testing-farm-tag-repository
     sudo dnf install -y podman
     echo "??????????"
     podman --version
