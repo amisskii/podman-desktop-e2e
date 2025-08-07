459f8c10a2713657cb5c8da3676e7fd3f50b958a
Anton Misskii <124462506+amisskii@users.noreply.github.com>
2025-08-07 15:12:36 +0200

    Update install-podman.sh

--- plans/scripts/install-podman.sh
+++ plans/scripts/install-podman.sh
@@ -7,14 +7,9 @@ CUSTOM_PODMAN_URL="https://kojipkgs.fedoraproject.org//packages/podman/${PODMAN_
 if [[ $PODMAN_VERSION == "latest" ]]; then
     sudo dnf install -y podman
 else
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo $CUSTOM_PODMAN_URL
     curl -Lo podman.rpm "$CUSTOM_PODMAN_URL"
     sudo dnf install -y ./podman.rpm
     rm -f podman.rpm
-fi
-
-INSTALLED_PODMAN_VERSION="$(podman --version | cut -d ' ' -f 3)"
-
-if [[ $INSTALLED_PODMAN_VERSION != $PODMAN_VERSION ]]; then
-    echo "Podman version mismatch: expected $PODMAN_VERSION but got $INSTALLED_PODMAN_VERSION"
-    exit 1
 fi
